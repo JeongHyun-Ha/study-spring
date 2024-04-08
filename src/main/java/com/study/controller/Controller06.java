332759b0a4274ae18e3a047e608a225a8f692f79
JeongHyun Ha <aszx2024@naver.com>
2024-04-08 10:36:48 +0900

    2024-04-08 Commit

---
+++ src/main/java/com/study/controller/Controller06.java
@@ -0,0 +1,59 @@
+package com.study.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("main6")
+public class Controller06 {
+
+    @RequestMapping("sub1")
+    public void method1() {
+        System.out.println("Controller06.method1");
+    }
+
+    // /main6/sub2?abc=son&qwe=56
+    // /main6/sub2
+    // /main6/sub2?abc=lee
+    // /main6/sub2?qwe=44
+    @RequestMapping("sub2")
+    public void method2(@RequestParam(value = "abc", required = false) String param1,
+                        @RequestParam(value = "qwe", required = false) Integer param2) {
+        System.out.println("Controller06.method2");
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+    // /main6/sub3?name=kim&age=44
+    // /main6/sub3?name=lee
+    // /main6/sub3?age=55
+    // todo: method3이 실행되도록 작성
+    @RequestMapping("sub3")
+    public void method3(@RequestParam(value = "name", required = false) String param1,
+                        @RequestParam(value = "age", required = false) Integer param2) {
+        System.out.println("param1 = " + param1);
+        System.out.println("param2 = " + param2);
+    }
+
+    // /main6/sub4?name=손흥민&age=33
+    // /main6/sub4?name=이강인
+    // /main6/sub4?age=55
+    @RequestMapping("sub4")
+    public void method4(@RequestParam(required = false) String name,
+                        @RequestParam(required = false) Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+
+    // /main6/sub5?address=shinchon&city=seoul
+    // /main6/sub5?city=busan
+    // /main6/sub5?address=강남
+    // todo: method5 작성
+
+    @RequestMapping("sub7")
+    public void method7(String name, Integer age) {
+        System.out.println("name = " + name);
+        System.out.println("age = " + age);
+    }
+}
